ef559b20e95e142fadbb99f3447d2a450e99726a
jagdish-crossml <jagdish@crossml.com>
2021-08-16 11:02:23 +0530

    TODO

---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,59 @@
+
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+
+from .models import *
+from .forms import *
+from .models import Task
+# Create your views here.
+
+
+def index(request):
+    tasks = Task.objects.all()
+    
+    form = TaskForm()
+
+    if request.method == 'POST':
+        print(request.POST)
+
+        title = request.POST.get('title')
+        Task(name=title).save()
+        # form = TaskForm(request.POST)
+        # if form.is_valid():
+        #     form.save()
+
+
+        return redirect('/')
+
+    else:
+        context = {'tasks': tasks, 'form': form}
+        return render(request, 'todoapp/ui.html', context)
+
+    
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, 'todoapp/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'todoapp/delete.html', context)
